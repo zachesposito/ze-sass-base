825ec0572d9ba54e51aaae2320048e1a73958177
Zach Esposito <zesposito@thermatru.com>
2018-06-25 13:34:45 -0400

    Get bourbon from npm instead of bower

--- README.md
+++ README.md
@@ -5,6 +5,4 @@ Depends on [Bourbon].
 
 [Bourbon]: https://github.com/thoughtbot/bourbon
 
-Add to your project with `@import (bower_components path)/ze-sass-base/ze-sass-base.scss`.
-
 `npm test` to run function & mixin tests.
--- test/test-sass.js
+++ test/test-sass.js
@@ -1,5 +1,15 @@
 var path = require('path');
 var sassTrue = require('sass-true');
+var tildeImporter = require('node-sass-tilde-importer');
 
 var sassFile = path.join(__dirname, 'all.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
+
+function importer(url, prev, done) {
+    if (url[0] === '~') {
+      url = path.resolve('node_modules', url.substr(1));
+    }
+   
+    return { file: url };
+  }
+   
+  sassTrue.runSass({ importer, file: sassFile }, describe, it);
